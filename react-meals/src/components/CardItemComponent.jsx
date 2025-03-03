120cdced9aaa5d3858d92d436ffb35f9fa45798e
daggel100 <da-lehwald@oulook.de>
2025-03-03 12:02:33 +0100

    ende meals

--- react-meals/src/components/CardItemComponent.jsx
+++ react-meals/src/components/CardItemComponent.jsx
@@ -1,5 +1,8 @@
 
 import { Link } from "react-router-dom"
+import { useEffect } from "react";
+
+
 
 const CardItemComponent = ({food}) => {
   return (
@@ -7,21 +10,21 @@ const CardItemComponent = ({food}) => {
 
     <div className="card bg-base-100 w-96 shadow-sm">
 
+<Link to={`/meals/${food.idMeal}`}>
+
   <figure>
     <img
       src={food.strMealThumb}
       alt={food.strMeal} />
   </figure>
   <div className="card-body">
-    <Link to={`/meals/${food.idMeal}`}>
     
- 
     <h2 className="card-title">
       {/* Card Title */}
       {food.strMeal}
       {/* <div className="badge badge-secondary">NEW</div> */}
     </h2>
-    </Link>
+  
     
     {/* <p>A card component has a figure, a body part, and inside body there are title and actions parts</p> */}
     <div className="card-actions justify-end">
@@ -29,6 +32,10 @@ const CardItemComponent = ({food}) => {
       <div className="badge badge-outline">{food.strArea}</div>
     </div>
   </div>
+  </Link>
+
+    
+
 </div>
   )
 }
--- react-meals/src/page/MealsDetailsPage.jsx
+++ react-meals/src/page/MealsDetailsPage.jsx
@@ -1,21 +1,68 @@
 
 import { useParams, Link } from "react-router-dom";
+import { fetchData } from "../utileties/apiFetch.js";
+import { useEffect, useState } from "react";
 
 
 
 const MealsDetailsPage = () => {
 
+
+
   const {id} = useParams();
+
+  const  [meal, setMeal] = useState([]);
+
+
+  const mealUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  useEffect(() => 
+    {
+      async function fetchMeals() 
+      {
+        try 
+        {
+          const data = await fetchData(mealUrl);
+          console.log("Test",data.meals[0]);
+          
+          setMeal(data.meals[0]);
+        } 
+        catch (error) 
+        {
+          console.error( error.message);
+        }
+      }
+  
+      fetchMeals();
+    }, [id]) 
+
 
   return (
 
-    <div>
-      MealsDetailsPage für {id}
-      <br />
-      <button className="btn btn-primary">
-        <Link to="/meals">Zurück</Link>
-      </button>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row gap-8">
+        <img
+          src={meal.strMealThumb}
+          alt={meal.strMeal}
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold mb-3">{meal.strMeal}</h1>
+          <p className="py-6 flex flex-col">
+            {meal.strInstructions &&
+              meal.strInstructions
+                .split(".")
+                .filter((instruction) => instruction.trim() !== "")
+                .map((instruction, index) => {
+                  return <span key={index}>- {instruction}</span>;
+                })}
+          </p>
 
+          <button className="btn btn-primary">
+            <Link to="/meals">Back to Meals</Link>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
